3d9cdc70105997ef80d18aecb4f28b55474db947
asdfghjA1 <aaryan14decembersingh@gmail.com>
2025-02-01 04:38:12 +0530

    take 4

--- app/chatbot-iframe/[chatID]/layout.tsx
+++ app/chatbot-iframe/[chatID]/layout.tsx
@@ -1,15 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import React from "react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function ChatbotIframeLayout({
     children,
